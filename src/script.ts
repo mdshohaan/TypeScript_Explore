d31e715e14e7bf34cc0c670188b89d25af2c8a51
mdshohaan <irfanulislamdu@gmail.com>
2025-02-28 18:34:32 +0600

    class constructor type

--- src/script.ts
+++ src/script.ts
@@ -37,6 +37,22 @@ myFunc = () => {
 };
 
 const weFunc = (a: string, b: number) => {
-  console.log(`this ${a} ${b}`);
+  return a + b;
 };
 weFunc("asssa", 12);
+
+class Player {
+  name: string;
+  age: number;
+  country: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+  play() {
+    console.log(`${this.name},${this.country}`);
+  }
+}
+const sakib = new Player("sakib", 52, "BD");
