06d3b804490847e62bcd960c4943adc6b1c891f4
mdshohaan <irfanulislamdu@gmail.com>
2025-02-28 16:16:09 +0600

    set the dynamic types

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,32 @@
 // normal variable
 let a: string | number;
 a = 5;
+
 // array
 let arr: (number | string)[] = [];
 arr.push("sumit", 5);
+
+// obj
+let c: {
+  name: string;
+  age: number;
+  adult: boolean;
+};
+c = {
+  name: "sohan",
+  age: 23,
+  adult: true,
+};
+// dynamic types
+let b: any[] = [];
+b.push(5);
+b.push("aaaa");
+// object dynamic
+let obj: {
+  name: any;
+  age: any;
+};
+obj = {
+  name: "afsa",
+  age: "twenty",
+};
